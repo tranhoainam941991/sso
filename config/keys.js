b764a99a9a25ea1a352928657e82f52f4f33a65c
Tran Hoai Nam <keolac941991@gmail.com>
2019-06-24 14:09:52 +0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '1014484472901-ksjs12dbdtq32eosobltvknsgbs3rm24.apps.googleusercontent.com',
+  googleClientSecret: 'EFZGTm6HWB0MSmaioepE5ZyN',
+  mongoURI: 'mongodb+srv://admin:abcd1234@cluster0-j1pte.mongodb.net/udemyFullStackJS?retryWrites=true&w=majority',
+  cookieKey: 'dasdasdasdasffgsdfwereqwradasdas123123'
+}
