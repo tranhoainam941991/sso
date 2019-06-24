0e0b0d3c681dd6dc9dd9af4d8f27ca07d5ffc980
Tran Hoai Nam <keolac941991@gmail.com>
2019-06-24 14:46:07 +0700

    dev and prod

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  googleClientID: '1014484472901-ksjs12dbdtq32eosobltvknsgbs3rm24.apps.googleusercontent.com',
-  googleClientSecret: 'EFZGTm6HWB0MSmaioepE5ZyN',
-  mongoURI: 'mongodb+srv://admin:abcd1234@cluster0-j1pte.mongodb.net/udemyFullStackJS?retryWrites=true&w=majority',
-  cookieKey: 'dasdasdasdasffgsdfwereqwradasdas123123'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
 }
